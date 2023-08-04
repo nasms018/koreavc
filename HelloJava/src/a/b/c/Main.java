2888db1d50f95579edc3aee4ff742c3d564dd9fd
차재민 <103953394+nasms018@users.noreply.github.com>
2023-08-04 17:19:25 +0900

    Add files via upload

---
+++ HelloJava/src/a/b/c/Main.java
@@ -0,0 +1,23 @@
+package a.b.c;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("sdfgdffgjgtrfttg dcfdfrf");
+		System.out.println("sdfgdfgtr4576fttg dcfdfrf");
+		System.out.println("sdfgdfgtrft54tg dcfdfrf");
+		System.out.println("sdfgdfgtrftt5478635g dcfdfrf");
+		System.out.println("sdfgdfgtrfttg dcfdfrf");
+		
+		System.out.println("sdfgdfgtrfttg dcfdfrf");
+		System.out.println("sdfgdffgjgtrfttg dcfdfrf");
+		System.out.println("sdfgdfgtr4576fttg dcfdfrf");
+		System.out.println("sdfgdfgtrft54tg dcfdfrf");
+		System.out.println("sdfgdfgtrftt5478635g dcfdfrf");
+		System.out.println("sdfgdfgtrfttg dcfdfrf");
+		
+		System.out.println("sdfgdfgtrfttg dcfdfrf");
+		System.out.println("sdfgdffgjgtrfttg dcfdfrf");
+		System.out.println("sdfgdfgtr4576fttg dcfdfrf");
+		System.out.println("sdfgdfgtrft54tg dcfdfrf");
+	}
+}
